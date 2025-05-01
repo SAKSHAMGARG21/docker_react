2407c0d57363e4718f8716c73f41efe30a575c40
SAKSHAMGARG21 <sakshamgarggarg83@gmail.com>
2025-05-01 20:31:47 +0530

    ci/cd implemention

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# # Dockerfile
+# FROM node:18-alpine AS builder
+
+# WORKDIR /app
+# COPY package*.json ./
+# RUN npm install
+# COPY . .
+# RUN npm run build
+
+# FROM nginx:alpine
+# COPY --from=builder /app/dist /usr/share/nginx/html
+# EXPOSE 80
+# CMD ["nginx", "-g", "daemon off;"]
+
+FROM node 
+
+WORKDIR /app
+COPY . .
+
+RUN npm install
+
+EXPOSE 3000
+
+CMD [ "npm","run","dev" ]
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      {/* <h1>Hellow Aws Docker kubernates is Going On</h1> */}
+      {/* <h2>My ReactJs app is working</h2> */}
+      {/* <h2>Docker and kubernates</h2> */}
+      <h1>Welcome to react on ECR</h1>
+      <p>Deploy by Saksham Garg</p>
+    </div>
+  )
+}
+
+export default App
